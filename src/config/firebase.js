c159842120fb1e32a400807b824389e56435a8cb
Carlos Moreno <dmisterjam@gmail.com>
2023-09-02 19:03:16 -0400

    ajustes finales

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,15 +3,15 @@ import { initializeApp } from "firebase/app";
 
 import {getFirestore} from  'firebase/firestore'
 
-
+// No pude poner el archivo .env, cuando lo intento me sale un error de Firebase.
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
-  measurementId: process.env.REACT_APP_measurementId
+  apiKey: "AIzaSyCqRVKbBmIy4yVKal42B0hlsHD4oqFO5_g",
+  authDomain: "proyecto-46516.firebaseapp.com",
+  projectId: "proyecto-46516",
+  storageBucket: "proyecto-46516.appspot.com",
+  messagingSenderId: "725247373919",
+  appId: "1:725247373919:web:17ec8a960efbc3fc2a21ea",
+  measurementId: "G-HPEJ7K6ES0"
 };
 
 
